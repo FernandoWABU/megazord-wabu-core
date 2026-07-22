84e21f04e3cdd9bbe9cfcbaabb60ba9bd0ead049
FernandoWABU <botellofernando77@gmail.com>
2026-07-21 20:31:53 -0600

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-import os
-
-# Asegurarse que PORT está definido
-os.environ.setdefault('PORT', '8000')
-
-# Ejecutar el webhook
-exec(open('src/webhook_simple.py').read())
